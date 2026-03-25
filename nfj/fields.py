beed114fb603c41be4f8fe1a592e6006e345cf7d
shingo405nagano <makishin.nagano@gmail.com>
2026-03-25 00:50:54 +0000

    [Change] FieldInfoにオリジナルの名称を追加  - `FieldInfo`: org を追加  - `_AddrsColumns`: geospatail.py -> fields.py へ移動

--- nfj/fields.py
+++ nfj/fields.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional
+from typing import Any, NamedTuple, Optional
 
 import pydantic
 
@@ -12,22 +12,24 @@ class FieldInfo(pydantic.BaseModel):
     """属性情報を表すクラス
 
     Attributes:
+        org (str): 元の属性名（日本語）
         ja (str): 日本語の属性名
         en (str): 英語の属性名
         dtype (Any): データ型（string、geometry、integer、floatのいずれか）
         default (Any): デフォルト値
     """
 
+    org: str
     ja: str
     en: str
     dtype: Any
     default: Any
     agg: Optional[str] = pydantic.Field(default=None)
 
-    @pydantic.field_validator("ja", "en", mode="before")
+    @pydantic.field_validator("org", "ja", "en", mode="before")
     def validate_string(cls, v):
         if not isinstance(v, str):
-            raise ValueError("jaとenは文字列でなければなりません。")
+            raise ValueError("org、ja、enは文字列でなければなりません。")
         return v
 
     @pydantic.field_validator("dtype", mode="before")
@@ -57,9 +59,6 @@ class FieldInfo(pydantic.BaseModel):
         try:
             return self.dtype(value)
         except (ValueError, TypeError):
-            logger.warning(
-                f"値 '{value}' を {self.dtype} に変換できません。デフォルト値を使用します。"
-            )
             return self.default
 
 
@@ -89,7 +88,10 @@ class AddressFields(BaseFields):
     """G空間センターで公開されている、国有林の林小班区画データの属性管理クラスです。"""
 
     def __init__(self):
-        fields = {key: FieldInfo(**value) for key, value in ADDRESS_FIELDS.items()}
+        fields = {}
+        for key, data in ADDRESS_FIELDS.items():
+            data["org"] = key
+            fields[key] = FieldInfo(**data)
         super().__init__(fields=fields)
 
     def rename_dict_org_to_en(self) -> dict[str, str]:
@@ -108,17 +110,40 @@ class AddressFields(BaseFields):
         """英語の属性名のリストを返します。"""
         return [field_info.en for field_info in self.fields.values()]
 
-    def field_info(self, en_field_name: str) -> Optional[FieldInfo]:
+    def field_info(self, en_field_name: str) -> FieldInfo:
         """英語の属性名からFieldInfoを取得します。
 
         Args:
             en_field_name: 英語の属性名
 
         Returns:
-            FieldInfoオブジェクト、または見つからない場合はNone
+            FieldInfoオブジェクト
+
+        Raises:
+            ValueError: 指定された英語の属性名が見つからない場合。
         """
         for field_info in self.fields.values():
             if field_info.en == en_field_name:
                 return field_info
-        logger.warning(f"英語の属性名 '{en_field_name}' は見つかりませんでした。")
-        return None
+        raise ValueError(f"英語の属性名 '{en_field_name}' は見つかりませんでした。")
+
+
+class _AddrsColumns(NamedTuple):
+    """小班区画の処理にてよく使用する英名の属性名を定義するクラス。"""
+
+    city: str = ADDRESS_FIELDS.get("県市町村", {}).get("en", "city")
+    plan_area: str = ADDRESS_FIELDS.get("計画区", {}).get("en", "plan_area")
+    office: str = ADDRESS_FIELDS.get("森林管理署", {}).get("en", "office")
+    branch_office: str = ADDRESS_FIELDS.get("担当区", {}).get("en", "branch_office")
+    locality: str = ADDRESS_FIELDS.get("国有林名", {}).get("en", "locality")
+    main_address: str = ADDRESS_FIELDS.get("林班主番", {}).get("en", "main_address")
+    address: str = ADDRESS_FIELDS.get("林小班名称", {}).get("en", "address")
+    sub_address: str = ADDRESS_FIELDS.get("小班名", {}).get("en", "sub_address_name")
+    establishment_year: str = ADDRESS_FIELDS.get("樹立年度", {}).get(
+        "en", "establishment_year"
+    )
+    tree_age_1: str = ADDRESS_FIELDS.get("樹齢1", {}).get("en", "tree_age_1")
+    tree_age_2: str = ADDRESS_FIELDS.get("樹齢2", {}).get("en", "tree_age_2")
+    tree_age_3: str = ADDRESS_FIELDS.get("樹齢3", {}).get("en", "tree_age_3")
+    conservation: str = ADDRESS_FIELDS.get("保護林", {}).get("en", "conservation")
+    green_corridor: str = ADDRESS_FIELDS.get("緑の回廊", {}).get("en", "green_corridor")
